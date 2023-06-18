5a99b6eb66aed9999ee6d6ac9dc6026de65de386
Falak <falakgala09@gmail.com>
2023-06-18 22:48:55 +0530

    fix:babel plugin

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-restricted-globals */
 import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst } from 'workbox-strategies';
 
 // Precache and route the specified URLs
 precacheAndRoute(self.__WB_MANIFEST);
-
-// Add additional caching strategies and routes as needed
 
 // Cache the root URL and index.html during installation
 self.addEventListener('install', (event) => {
@@ -14,6 +14,25 @@ self.addEventListener('install', (event) => {
     })
   );
 });
+
+// Serve index.html for all navigation requests
+registerRoute(
+  ({ request }) => request.mode === 'navigate',
+  ({ event }) => {
+    return caches.match('/index.html');
+  }
+);
+
+// Cache images using a CacheFirst strategy
+registerRoute(
+  /\.(?:png|jpg|jpeg|svg|gif)$/,
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      // Optional: Apply cache size limits or expiration
+    ],
+  })
+);
 
 // Intercept fetch requests and respond with cached resources if available
 self.addEventListener('fetch', (event) => {
