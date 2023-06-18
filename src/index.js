176b463f2667ef8d61a061cb5f2f8edd92861a34
Falak <falakgala09@gmail.com>
2023-06-18 22:09:48 +0530

    added logo and modification for pwa

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(registration => {
-        console.log('Service Worker registered: ', registration);
-      })
-      .catch(error => {
-        console.log('Service Worker registration failed: ', error);
-      });
-  });
-}
+const registerServiceWorker = () => {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/public/service-worker.js')
+        .then(registration => {
+          console.log('Service Worker registered: ', registration);
+        })
+        .catch(error => {
+          console.log('Service Worker registration failed: ', error);
+        });
+    });
+  }
+};
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+registerServiceWorker();
+
+ReactDOM.render(<App />, document.getElementById('root'));
