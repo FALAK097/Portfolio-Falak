95a79c97b44c0a33770a6854c298da343cacf210
falak <falakgala09@gmail.com>
2023-08-02 22:37:26 +0530

    style:responsive portfolio & service_worker changes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { registerServiceWorker } from './serviceWorkerRegistration';
 import './index.css';
-
-const registerServiceWorker = () => {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
-        .then(registration => {
-          console.log('Service Worker registered: ', registration);
-        })
-        .catch(error => {
-          console.log('Service Worker registration failed: ', error);
-        });
-    });
-  }
-};
 
 registerServiceWorker();
 
