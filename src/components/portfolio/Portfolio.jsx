a5f3e27600c92d77547479ed2770e5c1aa8c3d07
falak <falakgala09@gmail.com>
2023-08-06 14:34:41 +0530

    added Wild Heart to portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,82 +1,80 @@
-import React, { useState, useEffect } from "react";
-import "./Portfolio.css";
-import IMG1 from "../../assets/P3.png";
-import IMG2 from "../../assets/P1.png";
-import IMG3 from "../../assets/project-3.png";
-import IMG4 from "../../assets/P2.png";
-import IMG5 from "../../assets/project-5.jpg";
-import IMG6 from "../../assets/project-6.jpg";
+import React, { useState, useEffect } from 'react';
+import './Portfolio.css';
+import IMG1 from '../../assets/P3.png';
+import IMG2 from '../../assets/P1.png';
+import IMG3 from '../../assets/project-3.png';
+import IMG4 from '../../assets/P2.png';
+import IMG5 from '../../assets/project-5.jpg';
+import IMG6 from '../../assets/project-6.jpg';
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: "Luscious - Food Recipe PWA",
-    category: "Web App",
+    title: 'Luscious - Food Recipe PWA',
+    category: 'Web App',
     content:
-      "Created a Food Recipe Progressive Web App (PWA) using Spoonacular API, React, React Router, and Framer Motion. Implemented features for recipe search, browsing, and detailed information, providing users with a seamless and interactive experience.",
-    github: "https://github.com/FALAK097/Luscious",
-    demo: "https://luscious-six.vercel.app/",
+      'Created a Food Recipe Progressive Web App (PWA) using Spoonacular API, React, React Router, and Framer Motion. Implemented features for recipe search, browsing, and detailed information, providing users with a seamless and interactive experience.',
+    github: 'https://github.com/FALAK097/Luscious',
+    demo: 'https://luscious-falak097.vercel.app/',
   },
   {
     id: 2,
     image: IMG2,
-    title: "Education Website",
-    category: "Web Design",
+    title: 'Education Website',
+    category: 'Web Design',
     content:
-      "Presenting a responsive multipage website for an educational institute, showcasing courses and built using HTML, SCSS, and JavaScript. Experience seamless navigation and explore the diverse educational opportunities with ease.",
-    github: "https://github.com/FALAK097/Educationwebsite",
-    demo: "https://falak097.github.io/Educationwebsite/",
+      'Presenting a responsive multipage website for an educational institute, showcasing courses and built using HTML, SCSS, and JavaScript. Experience seamless navigation and explore the diverse educational opportunities with ease.',
+    github: 'https://github.com/FALAK097/Educationwebsite',
+    demo: 'https://falak097.github.io/Educationwebsite/',
   },
   {
     id: 3,
     image: IMG3,
-    title: "Project 03",
-    category: "Web App",
-    content:
-      "Coming Soon",
-    github: "https://github.com",
-    demo: "",
+    title: 'Project 03',
+    category: 'Web App',
+    content: 'Coming Soon',
+    github: 'https://github.com',
+    demo: '',
   },
   {
     id: 4,
     image: IMG4,
-    title: "Educational Website",
-    category: "Web Design",
+    title: 'Educational Website',
+    category: 'Web Design',
     content:
-      "Experience a responsive educational website featuring a diverse course selection and engaging blog content, meticulously built using HTML and CSS.",
-    github: "https://github.com/FALAK097/EducationalWebsite",
-    demo: "https://falak097.github.io/EducationalWebsite/",
+      'Experience a responsive educational website featuring a diverse course selection and engaging blog content, meticulously built using HTML and CSS.',
+    github: 'https://github.com/FALAK097/EducationalWebsite',
+    demo: 'https://falak097.github.io/EducationalWebsite/',
   },
   {
     id: 5,
     image: IMG5,
-    title: "Project 05",
-    category: "Web Design",
+    title: 'Wild Heart',
+    category: 'Web Design',
     content:
-      "Coming Soon",
-    github: "https://github.com",
-    demo: "",
+      'WildHeart - A Website for Animal Charity is a website dedicated to promoting animal welfare and conservation efforts. The website provides information about various animal species, their habitats, and the importance of preserving biodiversity.',
+    github: 'https://github.com/FALAK097/WildHeart',
+    demo: 'https://falak097.github.io/WildHeart/',
   },
   {
     id: 6,
     image: IMG6,
-    title: "Project 06",
-    category: "Web App",
-    content:
-      "Coming Soon",
-    github: "https://github.com",
-    demo: "",
+    title: 'Project 06',
+    category: 'Web App',
+    content: 'Coming Soon',
+    github: 'https://github.com',
+    demo: '',
   },
 ];
 
 const Portfolio = () => {
-  const [item, setItem] = useState({ name: "All" });
+  const [item, setItem] = useState({ name: 'All' });
   const [projects, setProjects] = useState(data);
   const [active, setActive] = useState(0);
 
   useEffect(() => {
-    if (item.name === "All") {
+    if (item.name === 'All') {
       setProjects(data);
     } else {
       const newProjects = data.filter((project) => {
@@ -103,10 +101,10 @@ const Portfolio = () => {
               onClick={(e) => {
                 handleClick(e, index);
               }}
-              className={`${active === index ? "active-work" : ""
-                } portfolio__item`}
-              key={index}
-            >
+              className={`${
+                active === index ? 'active-work' : ''
+              } portfolio__item`}
+              key={index}>
               {item.name}
             </span>
           );
@@ -127,16 +125,14 @@ const Portfolio = () => {
                   rel="noreferrer"
                   href={github}
                   className="btn"
-                  target="_blank"
-                >
+                  target="_blank">
                   GitHub
                 </a>
                 <a
                   rel="noreferrer"
                   href={demo}
                   className="btn btn-primary"
-                  target="_blank"
-                >
+                  target="_blank">
                   Live Demo
                 </a>
               </div>
@@ -150,14 +146,14 @@ const Portfolio = () => {
 
 export const projectsNav = [
   {
-    name: "All",
+    name: 'All',
   },
   {
-    name: "Web App",
+    name: 'Web App',
   },
   {
-    name: "Web Design",
+    name: 'Web Design',
   },
 ];
 
-export default Portfolio
+export default Portfolio;
